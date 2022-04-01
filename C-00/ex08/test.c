489b682c9a04f1d50d543ced6787fb2889695cfe
Andrea Blankenstijn <ablanken@car5s1.42barcelona.com>
2022-04-01 11:21:38 +0200

    tests for C-00

--- C-00/ex08/test.c
+++ C-00/ex08/test.c
@@ -1,9 +1,12 @@
 #include "ft_print_combn.c"
+#include <stdio.h>
 
 int main(void)
 {
   ft_print_combn(1);
+  printf("\n");
   ft_print_combn(5);
+  printf("\n");
   ft_print_combn(10);
 
   return 0;
